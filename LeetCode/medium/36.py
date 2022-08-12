b570278d20d6706b342f1a3d8befa8bfaaabafc2
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-08-12 17:24:44 +0900

    8/12/2022

--- LeetCode/medium/36.py
+++ LeetCode/medium/36.py
@@ -1,6 +1,61 @@
-from typing import List
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        # each row 1 - 9
+        # each col 1 - 9
+        # each 3 x 3 has 1 - 9
+        n = len(board)
 
-
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # case 1: check rows
+        for row in range(n):
+            seen = set()
+            for col in range(n):
+                curr = board[row][col]
+                if curr != ".":
+                    if curr in seen:
+                        return False
+                    seen.add(curr)
         
+        # case 2: check cols
+        for col in range(n):
+            seen = set()
+            for row in range(n):
+                curr = board[row][col]
+                if curr != ".":
+                    if curr in seen:
+                        return False
+                    seen.add(curr)
+        
+        # case 3: check 3 x 3
+        for row in range(0, n, 3):
+            for col in range(0, n, 3):
+                seen = set()
+                for i in range(3):
+                    for j in range(3):
+                        curr = board[row + i][col + j]
+                        if curr != ".":
+                            if curr in seen:
+                                return False
+                            seen.add(curr)
+        
+        return True
+                        
+
+            
+    
+
+board = [
+    ["5","3","2",".","7",".",".",".","."]
+    ,["6","4","1","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]
+]
+print(Solution().isValidSudoku(board))
---
+++ LeetCode/medium/454.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type nums3: List[int]
+        :type nums4: List[int]
+        :rtype: int
+        """
+        # 0 <= i, j, k, l < n
+        # nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+        # dp solution?
+
+
+print(Solution().fourSumCount(nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]))
