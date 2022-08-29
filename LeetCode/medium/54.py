17a1062212bdeaa4a3ef2a5600bec524890199d1
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-08-29 19:30:20 +0900

    8/29/2022

--- LeetCode/medium/54.py
+++ LeetCode/medium/54.py
@@ -3,20 +3,32 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        ROWS, COLS = len(matrix), len(matrix[0])
-
-        for i in range(min(ROWS, COLS) - 1):
-            for col in range(i, COLS - i - 1):
-                print(matrix[i][col])
+        l, r, t, d = 0, len(matrix[0]) - 1, 0, len(matrix) - 1
+        result = []
+        
+        while l <= r and t <= d:
             
-            for row in range(i, ROWS - i - 1):
-                print(matrix[row][COLS - i - 1])
+            for i in range(l, r + 1):
+                result.append(matrix[t][i])
+            t += 1
+            if t > d: break
             
-            for col in range(COLS - i - 1, i, -1):
-                print(matrix[ROWS - i - 1][col])
+            for i in range(t, d + 1):
+                result.append(matrix[i][r])
+            r -= 1
+            if r < l: break
             
-            for row in range(ROWS - i - 1, i, -1):
-                print(matrix[row][i])
+            for i in range(r, l - 1, -1):
+                result.append(matrix[d][i])
+            d -= 1
+            if d < t: break
+            
+            for i in range(d, t - 1, - 1):
+                result.append(matrix[i][l])
+            l += 1
+            if l > t: break
+        
+        return result
 
 
 matrix = [[1, 2]]
